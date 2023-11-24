52cacf644968b7769029cc336fc76c54b1f69826
Ankush Tandon <122861187+Ankush-Tandon@users.noreply.github.com>
2023-11-24 19:32:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from "react";
 import '../styles/App.css';
 
@@ -12,15 +11,44 @@ class App extends Component {
     };
   }
 
+  handleNext = () => {
+    this.setState((prevState) => ({
+      index: Math.min(prevState.index + 1, prevState.slides.length - 1),
+    }));
+  };
+
+  handlePrev = () => {
+    this.setState((prevState) => ({
+      index: Math.max(prevState.index - 1, 0),
+    }));
+  };
+
+  handleRestart = () => {
+    this.setState({
+      index: 0,
+    });
+  };
+
   render() {
-    
     const { index, slides } = this.state;
+    const currentSlide = slides[index];
 
     return (
-      <></>
+      <div id="main">
+        <h1 id="title">{currentSlide.title}</h1>
+        <p id="text">{currentSlide.text}</p>
+        <button id="button-restart" onClick={this.handleRestart} disabled={index === 0}>
+          Restart
+        </button>
+        <button id="button-prev" onClick={this.handlePrev} disabled={index === 0}>
+          Prev
+        </button>
+        <button id="button-next" onClick={this.handleNext} disabled={index === slides.length - 1}>
+          Next
+        </button>
+      </div>
     );
   }
 }
 
 export default App;
-
